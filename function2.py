fd4ff543269841a50ae0609e41d7f78b0eec4be9
kimsungho <atomiceld@gmail.com>
2022-10-12 16:21:52 +0900

    함수연습3

--- function2.py
+++ function2.py
@@ -1,4 +1,7 @@
 # function2.py
+from re import X
+
+
 def intersect(prelist,postlist):
     result=[]
     for x in prelist:
@@ -7,3 +10,28 @@ def intersect(prelist,postlist):
     return result
 
 print(intersect("HAM","SPAM"))
+
+x=5
+def func1(a):
+    return a+x
+print(func1(1))
+def func2(a):
+    x=2
+    return a+x
+print(func2(1))
+
+wordlist=["J","A","M"]
+def change(x):
+    x[0]="H"
+change(wordlist)
+print("함수 호출후 :{0}".format(wordlist))
+
+
+wordlist=["J","A","M"]
+def change(x):
+    x1=x[:]
+    x1[0]="H"
+    print("함수내부:{0}".format(x1))
+    
+change(wordlist)    
+print("함수 호출후 :{0}".format(wordlist))
