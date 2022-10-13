1d36f89daf40ca37afa5049558827423e4b10c0f
kimsungho <atomiceld@gmail.com>
2022-10-13 15:15:59 +0900

    1

--- 상속01.py
+++ 상속01.py
@@ -8,14 +8,19 @@ class Person:
 
 class Student(Person):
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #명시적으로 부모 초기화 메서드 호출
+        Person.__init__(self,name,phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
+    #덮어쓰기
+    def printInfo(self):
+        print("Info(Name:{0}, Phone Number: {1})".format(
+            self.name, self.phoneNumber))
+        print("Info(subject:{0}, studentID: {1})".format(
+            self.subject, self.studentID))
 
 p = Person("전우치", "010-222-1234")
-s = Student("이순신", "010-111-1234", "컴공", "991122")
+s = Student("이순신", "010-111-1234", "빅데이터학과", "991122")
 p.printInfo()
 s.printInfo()
 
